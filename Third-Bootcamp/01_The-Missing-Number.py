98a5d16db6f689861e9d330d353b565213331172
Hasibullah Aman <hasib.aman7@gmail.com>
2023-09-15 06:49:27 +0430

    first soluation for missing numbere

--- Third-Bootcamp/01_The-Missing-Number.py
+++ Third-Bootcamp/01_The-Missing-Number.py
@@ -1,1 +1,15 @@
-print('it the missing number')
+outputList = []
+for number_of_testCase in range(int(input("Enter te number of test Case: "))):
+    length_ofArray = int(input("Enter the number of Element: "))
+    start_ofArray = 0
+    array_list = list(map(int, input("Enter the element of Array: ").split()))
+    array_list = sorted(array_list)
+    for check in range(0, len(array_list)):
+        if start_ofArray == array_list[check]:
+            start_ofArray += 1
+        else:
+            outputList.append(check)
+            break
+for i in outputList:
+    print(i)
+
