eb802878c2bcc33ec9ca36dbf9d6fabfc696e681
Hasibullah Aman <hasib.aman7@gmail.com>
2023-09-19 07:13:37 +0430

    solve 01_gift_carpet

---
+++ Second-Bootcamp/01_Gift-Carpet.py
@@ -0,0 +1,24 @@
+import re
+alpha_list = []
+favrit_word = set()
+
+def check():
+    for i in range(len(alpha_list)):
+        fov_word = alpha_list[i]
+        my_word = 'vika'
+        for x in range(len(fov_word)):
+            if fov_word[x] == my_word[x]:
+                favrit_word.add(fov_word[x])
+
+for i in range(int(input("Enter the number of test case: "))):
+    size = list(map(int, input('Enter the size of Carpet: ').split()))
+    if size[1] > 3:
+        for y in range(0, size[0]):
+            alpha_list.append(input("Enter the word: "))
+
+check()
+
+if len(favrit_word) == 4:
+    print('Yes')
+else:
+    print("No")
