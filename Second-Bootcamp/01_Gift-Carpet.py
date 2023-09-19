b1ecac89a9662a10e7f7b0dcfe715a611c5773f2
Hasibullah Aman <hasib.aman7@gmail.com>
2023-09-19 17:38:24 +0430

    update and near to soluation

--- Second-Bootcamp/01_Gift-Carpet.py
+++ Second-Bootcamp/01_Gift-Carpet.py
@@ -1,23 +1,25 @@
-import re
-alpha_list = []
-favrit_word = set()
+result_list = []
+number_of_testCase = int(input("Enter the number of test case: "))
+for _ in range(number_of_testCase):
+    alpha_list = []
+    favrit_word = set()
+    def check():
+        for i in range(len(alpha_list)):
+            fov_word = alpha_list[i]
+            my_word = 'vika'
+            for x in range(len(fov_word)):
+                if fov_word[x] == my_word[x]:
+                    favrit_word.add(fov_word[x])
 
-def check():
-    for i in range(len(alpha_list)):
-        fov_word = alpha_list[i]
-        my_word = 'vika'
-        for x in range(len(fov_word)):
-            if fov_word[x] == my_word[x]:
-                favrit_word.add(fov_word[x])
+    for i in range(number_of_testCase):
+        size = list(map(int, input('Enter the size of Carpet: ').split()))
+        for y in range(0, size[0]):
+            alpha_list.append(input("Enter the word: "))
+    check()
+    for i in range(number_of_testCase):
+        if len(favrit_word) == 4:
+            result_list.append("Yes")
+        else:
+            result_list.append("No")
 
-for i in range(int(input("Enter the number of test case: "))):
-    size = list(map(int, input('Enter the size of Carpet: ').split()))
-    for y in range(0, size[0]):
-        alpha_list.append(input("Enter the word: "))
-
-check()
-
-if len(favrit_word) == 4:
-    print('Yes')
-else:
-    print("No")
+print(*result_list)
