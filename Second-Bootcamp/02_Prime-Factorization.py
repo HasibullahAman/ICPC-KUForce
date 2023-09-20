b3d92ef57fd6f8e597cb61c3f2148177cb0f1f2d
Hasibullah Aman <hasib.aman7@gmail.com>
2023-09-20 10:42:25 +0430

    solve the 02_Prime-Factorization

--- Second-Bootcamp/02_Prime-Factorization.py
+++ Second-Bootcamp/02_Prime-Factorization.py
@@ -1,3 +1,12 @@
 
+final_output = []
 for _ in range(int(input("Enter the number of test Case: "))):
-    pass
+    result_list = []
+    number = int(input("Enter the number for finding the prime Factorization: "))
+    for i in range(2, number + 1):
+        while number % i == 0:
+            result_list.append(i)
+            number = number // i
+    final_output.append(result_list)
+for i in final_output:
+    print(*i)
