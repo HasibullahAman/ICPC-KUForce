70cc0e48cbead06a4d5042877e7905159c01e30c
Hasibullah Aman <hasib.aman7@gmail.com>
2023-12-09 05:47:40 +0430

    update

---
+++ Prepar-JobInterview/Python_Bitwise_Operators.py
@@ -0,0 +1,14 @@
+a = 5  # 0101 in binary
+b = 3  # 0011 in binary
+
+result_and = a & b  # Bitwise AND operation
+print(result_and)  # Output: 1 (0001 in binary)
+
+result_or = a | b  # Bitwise OR operation
+print(result_or)  # Output: 7 (0111 in binary)
+
+result_xor = a ^ b  # Bitwise XOR operation
+print(result_xor)  # Output: 6 (0110 in binary)
+
+result_not = ~a  # Bitwise NOT operation
+print(result_not)  # Output: -6 (complement of 0101 in binary)
