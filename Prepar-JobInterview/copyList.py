0355b139fd8cce5b781d0db8ecd8219c0f80e9fe
Hasibullah Aman <hasib.aman7@gmail.com>
2023-12-09 07:33:13 +0430

    update

---
+++ Prepar-JobInterview/copyList.py
@@ -0,0 +1,13 @@
+# name = ['Hasibullah','Elhama','Freshta','Zuhra','Hikmatullah']
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango",'carrot']
+#
+# neList = fruits.copy()
+# print(neList)
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x)
+
+print(list1)
