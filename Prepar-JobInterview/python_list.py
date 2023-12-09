cd81b8e7ecd52181d39f7b468d25441ae87d67a4
Hasibullah Aman <hasib.aman7@gmail.com>
2023-12-09 06:25:11 +0430

    update list

--- Prepar-JobInterview/python_list.py
+++ Prepar-JobInterview/python_list.py
@@ -1,4 +1,49 @@
+# if our added item more than range, it will be added and other move accordingly.
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:2] = ["blackcurrant", "watermelon"]
+# print(thislist)
 
 thislist = ["apple", "banana", "cherry"]
-thislist[1:2] = ["blackcurrant", "watermelon"]
-print(thislist)
+# thislist[1:3] = ["watermelon"]
+# print(thislist)
+
+
+# using insert method
+
+name = ['Hasibullah','Elhama','Freshta','Zuhra','Hikmatullah']
+name.insert(3,'Rohullah')
+# print(*name)
+name.append('Shikiba')
+# print(*name)
+# name.extend('Nawidullah')
+# print(*name)
+
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = {
+    'name':'Hasibullah',
+    'lastName':'Qasim'
+}
+thislist.extend(thistuple)
+# print(thislist)
+
+
+newNameList = name.remove("Elhama")
+print(name)
+
+# newNameList = name.pop(4)
+# print(name)
+
+# newNameList = name.pop()
+# print(name)
+
+del name[0]
+print(name)
+
+# we can completely delete a list using del
+# del name
+# print(name)
+# name.clear()
+# print(name)
+
+
