9b8e014cdbe488129ce98e2028cebd8504004937
Hasibullah Aman <hasib.aman7@gmail.com>
2023-12-10 07:54:27 +0430

    updat

---
+++ Prepar-JobInterview/OOP/__str__()function.py
@@ -0,0 +1,37 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+p1 = Person("John", 36)
+
+print(p1)
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name}({self.age})"
+
+
+p1 = Person("John", 36)
+
+print(p1)
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f'my name is {self.name} and Im {self.age} years old'
+
+
+p2 = Person("Elhama", 22)
+
+print(p2)
