85d4844e2c905cc719950678f9a7f8985dcf4d90
Hasibullah Aman <hasib.aman7@gmail.com>
2023-12-10 06:14:07 +0430

    update

--- Prepar-JobInterview/Dictionary.py
+++ Prepar-JobInterview/Dictionary.py
@@ -1,14 +1,18 @@
 mydict = {
     'name': 'Hasibullah',
-    'LastName' : 'Aman',
-    'age' : 23
+    'LastName': 'Aman',
+    'age': 23
 }
 
 # for i in mydict:
 #     print(mydict.keys())
 #     print(mydict.values())
 
-print(mydict.items())
+# print(mydict.items())
+# mydict['name'] = 'Ali Ahmadi'
+# print(mydict.items())
 
-
-
+mydict.update({'university': 'Kabul University'})
+# print(mydict)
+for x, y in mydict.items():
+    print(x, " : ", y)
