0a0c93a3a8ed695d8fb2ca0dabcdb0eb7c4240ac
Hasibullah Aman <hasib.aman7@gmail.com>
2023-12-10 08:02:27 +0430

    update

--- Prepar-JobInterview/OOP/__str__()function.py
+++ Prepar-JobInterview/OOP/__str__()function.py
@@ -30,8 +30,15 @@ class Person:
 
     def __str__(self):
         return f'my name is {self.name} and Im {self.age} years old'
+    def definationName(self):
+        print(f"this Person {self.name} is so good person")
+        print(f'she is {self.age} years old')
 
 
 p2 = Person("Elhama", 22)
 
-print(p2)
+p3 = Person("Qasim",54)
+p3.age = 45
+p3.definationName()
+
+# print(p2)
