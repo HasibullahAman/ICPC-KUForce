0224c0e02950fd2eadf1176c6efef61376cbd64d
Hasibullah Aman <hasib.aman7@gmail.com>
2023-12-10 05:48:58 +0430

    update

---
+++ Prepar-JobInterview/Dictionary.py
@@ -0,0 +1,14 @@
+mydict = {
+    'name': 'Hasibullah',
+    'LastName' : 'Aman',
+    'age' : 23
+}
+
+# for i in mydict:
+#     print(mydict.keys())
+#     print(mydict.values())
+
+print(mydict.items())
+
+
+
