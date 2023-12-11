f9989b2d3356bd0b318e1040bc1d3a55210b8e02
Hasibullah Aman <hasib.aman7@gmail.com>
2023-12-11 07:19:57 +0430

    update and add module chepter

--- Prepar-JobInterview/OOP/Module.py
+++ Prepar-JobInterview/OOP/Module.py
@@ -1,8 +1,12 @@
 import platform
-
+import willImort_module
 # by using this code we access to our OS!
 Os = platform.system()
 # print(Os)
 
 x = dir('libc_ver')
 print(x)
+
+# car2 = willImort_module.Vehicle()
+companyName = willImort_module.CompanyName
+carList = willImort_module.myCar()
--- Prepar-JobInterview/OOP/willImort_module.py
+++ Prepar-JobInterview/OOP/willImort_module.py
@@ -7,3 +7,12 @@ class Vehicle:
 
     def move(self):
         print(f'{self.name} moved')
+
+
+def myCar():
+    car1 = Vehicle('Benz', "S7", 'Black', 2)
+    car1.move()
+
+
+CarList = ['Benz', 'Mazda', 'Tunes', "Corolla"]
+CompanyName = 'HUA'
