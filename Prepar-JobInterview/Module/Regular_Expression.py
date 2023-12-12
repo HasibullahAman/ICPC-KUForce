db08f8b316e4034bc7d321adc9d696a3671c2396
Hasibullah Aman <hasib.aman7@gmail.com>
2023-12-12 06:58:30 +0430

    upadate Regex

--- Prepar-JobInterview/Module/Regular_Expression.py
+++ Prepar-JobInterview/Module/Regular_Expression.py
@@ -11,4 +11,66 @@ txt = 'the rain is spain'
 # search for word in text
 find = re.search('Protugal', txt)
 
-print(find)
+# if I want to split the text and retrieve a list
+catch = re.split('\s', txt)
+
+for i in catch:
+    # print(i)
+    pass
+# print(catch)
+# print(find)
+
+x = re.split("\s", txt, 6)
+# print(x)
+
+
+replac = re.sub('\s', "_", txt, 3)
+
+
+# print(replac)
+
+# text = 'the rain is 89spain'
+# find = re.search('[a-z]?[0-9]spain$',text)
+# print(find.span())
+
+
+class CheckPass:
+    def __init__(self, userID, userName, userPassword, userHint):
+        self.userID = userID
+        self.userName = userName
+        self.userPassword = userPassword
+        self.userHint = userHint
+
+    def userIDAssign(self):
+        pass
+        self.userIDList = []
+        self.userID = 1
+        for i in range(len(self.userIDList)):
+            if self.userIDList[i] == self.userID:
+                self.userID += 1
+            else:
+                self.userIDList.append(self.userID)
+
+
+
+    def check(self):
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
