be5854baa688ce370c70b10c04772abc49febc48
Hasibullah Aman <hasib.aman7@gmail.com>
2023-12-12 06:35:54 +0430

    update and add new code on module part Regex

--- Prepar-JobInterview/Module/Regular_Expression.py
+++ Prepar-JobInterview/Module/Regular_Expression.py
@@ -2,5 +2,13 @@ import re
 
 txt = 'the rain is spain'
 
-find = re.search("^the.*spain$",txt)
+# find = re.findall("^the.*spain$",txt)
+# find = re.findall('portugal', txt)
+
+# for chick and control with space
+# find = re.search('\s', txt)
+
+# search for word in text
+find = re.search('Protugal', txt)
+
 print(find)
