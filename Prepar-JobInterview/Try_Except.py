7918deeca88f3285885317784259be983e136f06
Hasibullah Aman <hasib.aman7@gmail.com>
2023-12-12 07:55:17 +0430

    update and add read and write part

---
+++ Prepar-JobInterview/Try_Except.py
@@ -0,0 +1,24 @@
+# try:
+#     print(x)
+# except:
+#     print("I will reuse an error")
+
+# x = 'hello'
+#
+# try:
+#   print(x)
+# except NameError:
+#   print("Variable x is not defined")
+# finaly:
+#   print("Something else went wrong")
+
+x = "hello"
+
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
+
+
